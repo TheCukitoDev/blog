403406c9e807e437a30571e06bc28a454050cc33
Gabriel Díaz Aguilera <gabriel.diaz.aguilera@gmail.com>
2024-12-19 16:32:44 +0000

    Format files

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,10 +2,10 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+import './src/env.js'
 
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-const config: NextConfig = {};
+const config: NextConfig = {}
 
-export default config;
+export default config
